e65616bd93040a576b8f33ece01328d0d7e92636
tuananh200904 <tuanlbuianh@gmail.com>
2025-07-04 19:22:38 +0700

    Add

---
+++ src/main/java/com/example/datn_sp25/DatnSp25Application.java
@@ -0,0 +1,13 @@
+package com.example.datn_sp25;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnSp25Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatnSp25Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datn_sp25/DatnSp25ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datn_sp25;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnSp25ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
